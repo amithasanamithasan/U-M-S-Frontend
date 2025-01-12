1134881a399abe67136c463986dac49353bc5270
amithasanamithasan <amit35-1773@diu.edu.bd>
2025-01-12 14:09:14 +0600

    Project setup typescript and Redux-Toolkit

---
+++ src/App.tsx
@@ -0,0 +1,31 @@
+import { useDispatch, useSelector } from "react-redux";
+
+import { decrement, increment } from "./app/features/counter/counterSlice";
+import { RootState } from "./app/store";
+
+function App() {
+  const count = useSelector((state: RootState) => state.counter.value);
+  const dispatch = useDispatch();
+
+  return (
+    <div>
+      <div>
+        <button
+          aria-label="Increment value"
+          onClick={() => dispatch(increment())}
+        >
+          Increment
+        </button>
+        <span>{count}</span>
+        <button
+          aria-label="Decrement value"
+          onClick={() => dispatch(decrement())}
+        >
+          Decrement
+        </button>
+      </div>
+    </div>
+  );
+}
+
+export default App;
